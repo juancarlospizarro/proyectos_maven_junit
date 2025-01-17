8a8c35f040931fedfe3560ba4e873b7033c914f9
juancarlospizarro <jcpizarroalonso@gmail.com>
2025-01-17 11:11:46 +0100

    Realizado pruebas unitarias clase Termometro

--- termometro-proyecto/src/test/java/com/endes/TermometroTest.java
+++ termometro-proyecto/src/test/java/com/endes/TermometroTest.java
@@ -6,29 +6,80 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TermometroTest {
 
-	@BeforeAll
+	Termometro termometro;
+	
+	/* @BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
 	}
-
+	*/
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		termometro = new Termometro(5);
 	}
 
-	@AfterEach
+	/* @AfterEach
 	void tearDown() throws Exception {
+	}
+	*/
+	
+	@Test
+	@DisplayName("Comprobar resultado correcto Fharenheit")
+	void testConvertirAFahrenheit() {
+		Double valorEsperado = 41.0;
+		Double valor = termometro.convertirAFahrenheit();
+		
+		assertEquals(valorEsperado, valor);
+	}
+	
+	@Test
+	@DisplayName("Temperatura Kelvin no válida")
+	void testTemperaturaKelvinNoValida() {
+		termometro = new Termometro(-300);
+		String mensajeEsperado = "La temperatura no puede estar por debajo del cero absoluto.";
+		Exception e = assertThrows(IllegalArgumentException.class, () -> termometro.convertirAKelvin());
+		String mensaje = e.getMessage();
+		
+		assertEquals(mensajeEsperado, mensaje);
+	}
+	
+	@Test
+	@DisplayName("Temperatura Kelvin válida")
+	void testTemperaturaKelvinValida() {
+		Double valorEsperado = 278.15;
+		Double valor = termometro.convertirAKelvin();
+		
+		assertEquals(valorEsperado, valor);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Ajustar temperatura correctamente")
+	void testAjustarTemperaturaCorrecta() {
+		Double valorEsperado = 25.0;
+		termometro.ajustarTemperatura(20);
+		Double valor = termometro.getTemperaturaCelsius();
+		
+		assertEquals(valorEsperado, valor);
 	}
-
+	
+	@Test
+	@DisplayName("La temperatura se encuentra en rango")
+	void testTemperaturaEnRango() {
+		assertTrue(termometro.estaEnRango(0, 10));
+	}
+	
+	@Test
+	@DisplayName("La temperatura no se encuentra en rango")
+	void testTemperaturaNoEnRango() {
+		assertFalse(termometro.estaEnRango(10, 20));
+	}
 }
